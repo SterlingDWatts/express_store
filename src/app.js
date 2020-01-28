bcc979d21872032117386244b4301e66c7ab0270
Sterling Watts <SterlingDWatts@gmail.com>
2020-01-27 23:02:55 -0800

    Add password and username validation to POST /user

--- src/app.js
+++ src/app.js
@@ -34,8 +34,20 @@ app.post("/user", (req, res) => {
     return res.status(400).send("Username required");
   }
 
+  if (username.length < 6 || username.length > 20) {
+    return res.status(400).send("Username must be between 6 and 20 characters");
+  }
+
   if (!password) {
     return res.status(400).send("Password required");
+  }
+
+  if (password.length < 8 || password.length > 36) {
+    return res.status(400).send("Password must be between 8 and 36 characters");
+  }
+
+  if (!password.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)) {
+    return res.status(400).send("Password must contain at least one digit");
   }
 
   if (!favoriteClub) {
